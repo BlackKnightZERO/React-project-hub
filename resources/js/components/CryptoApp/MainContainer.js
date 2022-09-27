e57e04e6d578238e2d60e6f68017010681cd48f5
BlackKnightZERO <ayon.arif.10@gmail.com>
2022-09-27 18:21:39 -0400

    crypto-app-updated

--- resources/js/components/CryptoApp/MainContainer.js
+++ resources/js/components/CryptoApp/MainContainer.js
@@ -19,22 +19,32 @@ const MainContainer = () => {
               </thead>
               <tbody>
                 <tr>
-                  <td rowSpan={2} className='crypto-app-body-table-row-span-element mobile-fild-hide'>
-                    1
-                  </td>
-                  <td rowSpan={2} className='crypto-app-body-table-row-span-element'>
-                    <img 
-                      className='crypto-app-body-table-row-logo'
-                      src="https://assets.coincap.io/assets/icons/btc@2x.png" 
-                    />
-                  </td>
-                  <td className='crypto-app-body-table-top-row'>Bitcoin</td>
-                  <td rowSpan={2} className='crypto-app-body-table-row-span-element mobile-fild-hide'>DATA</td>
-                  <td rowSpan={2} className='crypto-app-body-table-row-span-element mobile-fild-hide'>DATA</td>
-                  <td rowSpan={2} className='crypto-app-body-table-row-span-element crypto-app-hourly-change-rate'>0.73%</td>
+                    <td rowSpan={2} className='crypto-app-body-table-row-span-element mobile-fild-hide'>
+                      1
+                    </td>
+                    <td rowSpan={2} className='crypto-app-body-table-row-span-element'>
+                      <img 
+                        className='crypto-app-body-table-row-logo'
+                        src="https://assets.coincap.io/assets/icons/btc@2x.png" 
+                      />
+                    </td>
+                    <td className='crypto-app-body-table-top-row'>
+                      Bitcoin
+                    </td>
+                    <td rowSpan={2} className='crypto-app-body-table-row-span-element mobile-fild-hide'>
+                      DATA
+                    </td>
+                    <td rowSpan={2} className='crypto-app-body-table-row-span-element mobile-fild-hide'>
+                      DATA
+                    </td>
+                    <td rowSpan={2} className='crypto-app-body-table-row-span-element crypto-app-hourly-change-rate'>
+                      0.73%
+                    </td>
                 </tr>
                 <tr>
-                  <td className='crypto-app-body-table-bottom-row'>BTX</td>
+                    <td className='crypto-app-body-table-bottom-row'>
+                      BTX
+                    </td>
                 </tr>
               </tbody>
             </table>
